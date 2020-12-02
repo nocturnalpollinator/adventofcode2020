98c7e844db2f00d52d86d6fe32e1c0ec01f696e0
Robin <hejrobinjonsson@gmail.com>
2020-12-02 08:55:50 +0100

    Day 2

---
+++ 2/main.js
@@ -0,0 +1,26 @@
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('input.txt')
+})
+
+var setup = []
+lineReader.on('line', line => {
+    setup.push(line.replace(':', '')
+                 .replace('-', ' ')
+                 .split(' ')
+                 .map(item => isNaN(item) ? item : parseInt(item)))
+})
+
+lineReader.on('close', () => {
+    console.log('Part I: ' +
+        setup.filter(item => {
+            match = item[3].match((new RegExp(item[2], 'g')) || [])
+            return match !== null && match.length >= item[0] && match.length <= item[1]
+        }).length
+    )
+
+    console.log('Part II: ' +
+        setup.filter(item => {
+            return (item[3].charAt(item[0] - 1) == item[2] && item[3].charAt(item[1] - 1) != item[2]) ||  (item[3].charAt(item[0] - 1) != item[2] && item[3].charAt(item[1] - 1) == item[2])
+        }).length
+    )
+})
