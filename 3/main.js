c38a911c672450104ead45a6b6318c84fa969a49
Robin <hejrobinjonsson@gmail.com>
2020-12-03 08:40:55 +0100

    Day 3

---
+++ 3/main.js
@@ -0,0 +1,21 @@
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('input.txt')
+})
+
+x1 = x2 = x3 = x4 = 0
+oddLine = true
+count = [0,0,0,0,0]
+
+lineReader.on('line', line => {
+    count = [
+        count[0] + (line.charAt(Math.ceil(x1/2) % 31) == '#' && oddLine),
+        count[1] + (line.charAt((x1++) % 31) == '#'),
+        count[2] + (line.charAt(((x2 += 3) - 3) % 31) == '#'),
+        count[3] + (line.charAt(((x3 += 5) - 5) % 31) == '#'),
+        count[4] + (line.charAt(((x4 += 7) - 7) % 31) == '#')
+    ]
+    oddLine = !oddLine
+}).on('close', () => {
+    console.log('Part I: ' + count[2]);
+    console.log('Part II: ' + count.reduce((a, b) => a * b))
+})
