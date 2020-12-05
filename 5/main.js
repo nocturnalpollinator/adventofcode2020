c41d31b3006a35cb2b626b82cec3b9bd1d210455
Robin <hejrobinjonsson@gmail.com>
2020-12-05 11:26:43 +0100

    Made it ugly

--- 5/main.js
+++ 5/main.js
@@ -3,25 +3,14 @@ var lineReader = require('readline').createInterface({
 })
 
 function traverse(min, max, f) {
-    if (f.length == 1) {
-        return f == 'F' || f == 'L' ? min : max
-    } else {
-        x = f.shift()
-        return x == 'F' || x == 'L' ? 
-               traverse(min, Math.floor(max - ((max - min) / 2)), f) : 
-               traverse(Math.ceil(max - ((max - min) / 2)), max, f)
-    }
+    x = f.shift()
+    return !f.length ? 'FL'.includes(x) ? min : max : ('FL'.includes(x) ? traverse(min, Math.floor(max - ((max - min) / 2)), f) : traverse(Math.ceil(max - ((max - min) / 2)), max, f))
 }
 
-highest = 0;
 ids = []
 lineReader.on('line', line => {
-    id = (traverse(0, 127, line.substr(0, 7).split('')) * 8) + traverse(0, 7, line.substr(7, 3).split(''))
-    highest = id > highest ? id : highest 
-    ids.push(id)
+    ids.push((traverse(0, 127, line.substr(0, 7).split('')) * 8) + traverse(0, 7, line.substr(7, 3).split('')))
 }).on('close', () => {
-    console.log('Part I: ' + highest)
+    console.log('Part I: ' + ids.sort((a, b) => a - b)[ids.length - 1])
     console.log('Part II: ' + (ids.filter(item => (ids.indexOf(item+1) == -1 && ids.indexOf(item+2) != -1)).pop() + 1))
-                // console.log('Part II: ' + item)
-        // })
 })
